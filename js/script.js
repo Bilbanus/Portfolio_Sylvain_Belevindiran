e80190a7fb7544115884c2af09794261458a87d1
Bilbanus <mamadocsindu@gmail.com>
2026-02-12 00:38:00 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -6,6 +6,7 @@ const sections = {
                 title: 'SAE-303: Concevoir visualisation de données web support animé',
                 description: 'Dans le cadre de ce projet, j’ai réalisé quatre animations visant à illustrer les opinions des Français sur les gaz à effet de serre (GES), selon l’âge, le sexe et la région. Le projet s’inscrit dans une démarche de data visualisation animée afin de rendre des données statistiques plus accessibles au grand public. Pour faciliter la compréhension, j’ai regroupé les informations en deux thématiques principales, chacune déclinée en deux animations. La première animation porte sur les activités volcaniques, symbolisant une cause supposée des émissions de GES. Réalisées sur Photoshop, ces animations utilisent l’animation image par image : un volcan entre en éruption et des gouttes de lave affichent les opinions des Français. La deuxième animation porte sur les transports (moto, voiture, train, avion) et montre la formation d’un nuage de pollution affichant les perceptions régionales des Français.',
                 image: '../img/volcan.png',
+                icon : '',
                 link: 'https://docs.google.com/document/d/1zz4wYVwyyIX6BYeZ2Y4rfHejinCLTV-TY4TA-eqHVa8/edit?tab=t.0'
             },
             {
@@ -13,6 +14,7 @@ const sections = {
                 title: 'SAE 302: Produire des contenus pour la communication plurimédia',
                 description: 'Dans le cadre de la SAE 302, j’ai conçu une affiche de sensibilisation en triptyque contre les violences conjugales. Ce format permet de représenter les différentes victimes : femmes, hommes et enfants. Le concept repose sur la notion d’emprise, symbolisée par une main qui se referme progressivement, illustrant la domination psychologique exercée par l’agresseur. Chaque panneau met en scène une victime différente tout en conservant une cohérence visuelle forte.Mon objectif était de produire un visuel sobre, percutant et accessible, capable de transmettre un message fort sans recourir à une mise en scène choquante.',
                 image: '../img/afiche.png',
+                icon : '',
                 link: 'https://www.figma.com/design/EwmUOFTBfri4CWzWB4V92p/302?node-id=884-1023&p=f&t=kfgfyHwXu5jS8ENA-0'
             },
             {
@@ -20,6 +22,7 @@ const sections = {
                 title: 'Gestion-Projet: Logo Alumni – IUT Sorbonne Paris Nord',
                 description: 'Dans le cadre d’un projet scolaire, j’ai participé à la conception d’un réseau alumni destiné à l’Université Sorbonne Paris Nord. L’objectif était de créer une plateforme numérique pour renforcer les liens entre anciens étudiants, étudiants, enseignants et entreprises. La plateforme facilite les échanges professionnels, le partage d’offres de stages et d’alternances, ainsi que l’accompagnement des étudiants dans leur insertion.Dans ce projet, j’ai réalisé le logo du site, la charte graphique, les wireframes, les maquettes ainsi que la charte éditoriale.',
                 image: '../img/alumni.png',
+                icon : '',
                 link: 'https://www.figma.com/slides/QytsBQeomVK5r1dteeLU2b/Charte-Graphique?node-id=71-2&t=YuY3k2m77IwZpBIB-0'
             },
             {
@@ -27,6 +30,7 @@ const sections = {
                 title: 'Portfolio: Design de CV',
                 description: 'Dans le cadre de la création de mon portfolio et de mon profil LinkedIn, j’ai réalisé plusieurs styles de CV afin de proposer des versions adaptées à différents secteurs et objectifs professionnels. J’ai exploré diverses identités visuelles en variant les mises en page, les typographies et les palettes de couleurs, tout en conservant une lecture claire et une hiérarchisation efficace de l’information. L’objectif était de concevoir des CV à la fois esthétiques et fonctionnels, capables de valoriser un profil selon le contexte.',
                 image: '../img/cv.png',
+                icon : '',
                 link: 'https://www.figma.com/design/ohfKsqDHpplXqDs9pyJ617/CV-Design?node-id=2-519&t=uOpBRMUyMWUoC3YU-0'
             },
             {
@@ -34,6 +38,7 @@ const sections = {
                 title: 'Site Jimdo Solo Leveling',
                 description: 'Dans le cadre de mon apprentissage des CMS et de la création de sites avec Jimdo, j’ai réalisé un site de fans autour du manhwa Solo Leveling. Ce projet m’a permis de comprendre le fonctionnement d’un CMS, de structurer et gérer des contenus web, de personnaliser une interface existante et d’intégrer des animations graphiques pour améliorer l’expérience utilisateur. Le thème de Solo Leveling a servi de support créatif pour expérimenter le design, la mise en page et la cohérence graphique.',
                 image: '../img/solo.png',
+                icon : '',
                 link: 'https://solo-leveling.jimdofree.com'
             }
         ]
@@ -46,6 +51,7 @@ const sections = {
                 title: 'Site des Travaux Pratiques',
                 description: 'Ce site a été conçu comme un hub centralisant...',
                 image: '../img/pratique.png',
+                icon : '',
                 link: 'https://rendu-tp1-1zmfopcku-mamadocspignoufs-projects.vercel.app'
             },
             {
@@ -53,6 +59,7 @@ const sections = {
                 title: 'Intégration Web: Bannière',
                 description: 'Dans le cadre d’un exercice de développement web...',
                 image: '../img/gojo.png',
+                icon : '',
                 link: 'https://sylvainrendutp.vercel.app/tp6/exercice%205/index.html'
             },
             {
@@ -60,6 +67,7 @@ const sections = {
                 title: 'TP Noté Animation',
                 description: 'Interface interactive minimaliste dédiée à Jujutsu Kaisen...',
                 image: '../img/sukuna.png',
+                icon : '',
                 link: '../Gojo/Tpnote.html'
             },
             {
@@ -67,6 +75,7 @@ const sections = {
                 title: 'Site TP développement front',
                 description: 'Plateforme vitrine regroupant mes exercices...',
                 image: '../img/devfront.png',
+                icon : '',
                 link: 'https://sylvainrendutp.vercel.app/'
             }
         ]
@@ -78,21 +87,24 @@ const sections = {
                 id: 11,
                 title: 'Maquette du site Solo Leveling',
                 description: 'Maquette graphique et interactive sur Figma...',
-                image: '../img/solo.png',
+                image: '../img/maquette.png',
+                icon : '',
                 link: 'https://www.figma.com/design/oYl5hVqFqh6wBg3rnAsvRS/Site-Jimdo-Solo-leveling?node-id=0-1&p=f&t=8XEJBUUQ1v3SdR1F-0'
             },
             {
                 id: 12,
-                title: 'SAE 202 – Kanoguti Experience',
+                title: 'Maquette – Kanoguti Experience',
                 description: 'Création d’une exposition immersive...',
-                image: '../img/black.png',
+                image: '../img/maquette.png',
+                icon : '',
                 link: 'https://www.figma.com/design/yBk32O0zwBt5JRoGs3OSvv/Alumni-site-web?node-id=10-2&p=f&t=ynNJKWLLUhbZPOZM-0'
             },
             {
                 id: 13,
                 title: 'Maquette Wireframes Alumni',
                 description: 'Wireframe d’un réseau alumni...',
-                image: '../img/alumni.png',
+                image: '../img/maquette.png',
+                icon : '',
                 link: 'https://www.figma.com/design/4nd83WXK3xHAeZPHbBL5Xb/kanaguti-maket?node-id=0-1&p=f&t=8XEJBUUQ1v3SdR1F-0'
             },
             {
@@ -100,14 +112,16 @@ const sections = {
                 title: 'Stratégie de communication',
                 description: 'Analyse de la stratégie autour de la réforme des retraites...',
                 image: '../img/Doc.png',
+                icon : '',
                 link: 'https://docs.google.com/document/d/1dAnAkmCljnnRnyCTCfWH0hoFnFZ1iaaiege7zWAfp2A/edit?tab=t.0'
             },
             {
                 id: 15,
-                title: 'Charte graphique SAE 202',
-                description: 'Création d’une charte graphique complète...',
-                image: '../img/coeur.png',
-                link: 'https://www.figma.com/design/ujsU5Jvxry6Kl4zThcLjG8/Untitled?node-id=0-1&p=f&t=ye0Jy2vjMJRDNrJL-0'
+                title: 'Charte graphique Monogramme',
+                description: 'Création d’une charte graphique complète',
+                image: '../img/maquette.png',
+                icon : '',
+                link: 'https://www.figma.com/slides/FLp1k6ANfZpkSmWAZq13SY/Chatre-graphique-sylvain?node-id=2-62&t=0PojRFsxGi9sYBBM-0'
             }
         ]
     }
@@ -171,7 +185,10 @@ function prevSlide(section) {
 // --- MODALE ---
 function openModal(project) {
     const modal = document.getElementById('modal');
-    document.getElementById('modal-icon').innerHTML = `<img src="${project.image}" alt="${project.title}">`;
+    const modalImage = document.getElementById('modal-image');
+    modalImage.src = project.icon;
+    modalImage.alt = project.title;
+
     document.getElementById('modal-title').textContent = project.title;
     document.getElementById('modal-description').textContent = project.description;
 
